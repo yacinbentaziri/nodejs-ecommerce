886f78ff461ea0e7b9049dd1ee3ee2ea96be9be1
yacinbtz <yacin.bentaziri@gmail.com>
2023-10-27 19:58:16 +0200

    first commit

--- routes/payment.js
+++ routes/payment.js
@@ -3,8 +3,8 @@ const router = express.Router()
 import { send, verify } from "../controllers/paymentController.js"
 import { verifyAccessToken, verifyUser } from "../utils/verifyToken.js"
 
-router.post("/send/:id", verifyAccessToken, verifyUser, send)
-router.post("/verify/:paymentid/:id", verifyAccessToken, verifyUser, verify)
+router.post("/send/:id", send)
+router.post("/verify/:paymentid/:id", verify)
 
 
 export default router
